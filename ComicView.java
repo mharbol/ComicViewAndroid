e7a2801758c4f5e7494895c9a1c13b552af069e6
mharbol <mharbol@citadel.edu>
2017-04-17 18:45:07 -0400

    started work on ComicView

--- ComicView.java
+++ ComicView.java
@@ -6,13 +6,15 @@ import android.widget.ListView;
 
 public class ComicView extends ListView {
 
+
+
     // constructors to make compiler and IDE happy
     // allows XML to stylize ComicView
     public ComicView(Context context){
-        super(context);
+        this(context, null, 0);
     }
     public ComicView(Context context, AttributeSet attrs){
-        super(context, attrs);
+        this(context, attrs, 0);
     }
     public ComicView(Context context, AttributeSet attrs, int defStyle){
         super(context, attrs, defStyle);
