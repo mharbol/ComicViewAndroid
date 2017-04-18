3ea678c0ae96e66d2f3f3ed59b4eb3d267aadaa1
mharbol <mharbol@citadel.edu>
2017-04-17 20:12:48 -0400

    update ComicView to save and restore state

--- ComicView.java
+++ ComicView.java
@@ -1,11 +1,51 @@
 package edu.citadel.android.comicviewtester;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
+
 public class ComicView extends ListView {
 
+    private ArrayList<ComicGetter> comicGetters;
+
+    public void addComics(ArrayList<String> comics){
+
+        // make ArrayList of ComicGetters to add to ListView
+        comicGetters = new ArrayList<>();
+        for (String comicName : comics)
+            comicGetters.add(new ComicGetter(comicName));
+
+        addComicGetters();
+    }
+
+    private void addComicGetters(){
+        this.setAdapter(new ComicAdapter(this.getContext(), comicGetters));
+    }
+
+    // Custom View version of saving and restoring instance state
+    @Override
+    public Parcelable onSaveInstanceState(){
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("superState", super.onSaveInstanceState());
+        bundle.putSerializable("comicGetters", comicGetters);
+        return bundle;
+    }
+
+    @Override
+    public void onRestoreInstanceState(Parcelable inState){
+        if (inState instanceof Bundle){
+            Bundle bundle = (Bundle) inState;
+            comicGetters = (ArrayList<ComicGetter>) bundle.getSerializable("comicGetters");
+            addComicGetters();
+            inState = bundle.getParcelable("superState");
+        }
+        super.onRestoreInstanceState(inState);
+    }
 
 
     // constructors to make compiler and IDE happy
--- MainActivity.java
+++ MainActivity.java
@@ -2,54 +2,31 @@ package edu.citadel.android.comicviewtester;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView lv;
-    private ArrayList<ComicGetter> cgal;
+    private ComicView cv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // originally made with array but ArrayList will be more practical
-        ComicGetter[] cg = {new ComicGetter("Calvin and Hobbes"),
-                new ComicGetter("Pearls Before Swine"),
-                new ComicGetter("Wizard of Id"),
-                new ComicGetter("B.C."),
-                new ComicGetter("Garfield"),
-                new ComicGetter("Peanuts"),
-                new ComicGetter("Non Sequitur"),
-                new ComicGetter("Andy Capp")};
+        cv = (ComicView) findViewById(R.id.activity_main);
 
-        cgal = new ArrayList<>();
-        for (ComicGetter c : cg)
-            cgal.add(c);
+        ArrayList<String> comics = new ArrayList<>();
+        comics.add("Calvin and Hobbes");
+        comics.add("Pearls Before Swine");
+        comics.add("Wizard of Id");
+        comics.add("B.C.");
+        comics.add("Garfield");
+        comics.add("Peanuts");
+        comics.add("Non Sequitur");
+        comics.add("Andy Capp");
 
-        // setting the adapter for the ListView.  In the future this should
-        // be its own ComicView object that does this all automatically.
-        lv = (ListView) findViewById(R.id.activity_main);
-
-        lv.setAdapter(new ComicAdapter(this, cgal));
-
+        cv.addComics(comics);
 
     }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState){
-        outState.putSerializable("cgal", cgal);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle inState){
-        super.onRestoreInstanceState(inState);
-        cgal = (ArrayList<ComicGetter>) inState.getSerializable("cgal");
-        lv.setAdapter(new ComicAdapter(this, cgal));
-    }
-
 }
